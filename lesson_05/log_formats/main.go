5a3417134bbea61ec7635fef389d2baea1b2fa6a
make-it-git <make.it.git@gmail.com>
2025-05-24 13:02:19 +0200

    Lesson 5: log formats example

---
+++ lesson_05/log_formats/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+)
+
+// go run main.go | jq
+// ENV=prod go run main.go
+func main() {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+
+	var appEnv = os.Getenv("ENV")
+
+	var handler slog.Handler = slog.NewTextHandler(os.Stdout, opts)
+	if appEnv == "prod" {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	}
+
+	logger := slog.New(handler)
+
+	logger.LogAttrs(
+		context.Background(),
+		slog.LevelWarn,
+		"User is a potential scammer",
+		slog.Int("id", 12345),
+		slog.String("scam_action", "withdraw_money"),
+		slog.Group("properties",
+			slog.Int("amount", 100500),
+			slog.String("currency", "USD"),
+		),
+	)
+}
